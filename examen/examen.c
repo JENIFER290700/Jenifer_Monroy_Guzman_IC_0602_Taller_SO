8fed345776d5f591ed67ee63c162eb68e493d5ee
jeni monroy <jenifermon29@gmail.com>
2021-05-19 09:01:29 -0500

    examen practico

--- examen/examen.c
+++ examen/examen.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int  main(){
+char x;
+char *p;
+char y;
+char *puntero;
+x='x';
+p=&x;
+printf("\nEL CONTENIDO DEL PUNTERO P ES: %c",*p);
+p++;
+printf("\nEL CONTENIDO DEL PUNTERO P + 1 ES: %c",*p);
+p+=2;
+printf("\nEL CONTENIDO DEL PUNTERO P + 2 ES: %c",*p);
+y='y';
+puntero=&y;
+printf("\nEL CONTENIDO DEL PUNTERO ES: %c\n",*puntero);
+return 0;
+}
