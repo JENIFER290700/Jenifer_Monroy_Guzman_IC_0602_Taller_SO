1ccb809e995f4bf52bf1a27d53db193395557598
jeni monroy <jenifermon29@gmail.com>
2021-06-09 15:25:09 -0500

    manejo de archivos

--- archivos/control.c
+++ archivos/control.c
@@ -2,17 +2,47 @@
 #include <string.h>
 
 #define tam_nombre 100
-#define tam_nombre2 100
+#define tam_lectura 100
+
 
 int crea_arch(char *nom_arch);
 int datos_archivo(char *nom_arch);
-FILE *abrir_arch(char *nom_arch);
+
+FILE *abrir_arch(char *nom_arch); {
+	FILE *ptrArch;
+	ptrArch = fopen(nom_arch, "r");
+	if (ptrArch == NULL) {
+		printf("\n\t El archivo no pudo abrirse : %s\n", nom_arch);
+	}
+	else{
+		printf ("\n\t Archivo abierto : %s", nom_arch);
+	
+	}
+	return ptrArch;
+}
+void c_archivo(FILE *ptrArch,char *nom_arch){
+fclose(ptrArch);
+printf("\n\tArchivo cerrado\n\t")
+}
+
+int exis_Arch(char *nom_arch) {
+	FILE *ptrArch;
+	int ex = 0;
+	ptrArch = fopen(nom_arch, "r");
+	if(ptrArch != NULL) {
+	existe=1;
+	c_archivo(ptrArch, nom_arch);
+	}
+	return existe;
+}
 
 
-int main(int argc, char *argv[]) {
+int main(int argc, char const *argv[]) {
+	
 	char or[TAM_NOMBRE];
 	char dest [TAM_NOMBRE];
 	int total_Palabras = 0, cont;	
+	
 	
 	if (argc != 3){
 		printf("No existen los argumentos revisalos \n");
@@ -32,7 +62,7 @@ int main(int argc, char *argv[]) {
 	if(archivo(or)){
 	printf("\tEL ARCHIVO CONTIENE DATOS : %s\n", or);
 	total_Palabras = archivo(or);	
-	printf("TODOS LOS ELEMENTOS [%d]\n\n", 			total_Palabras);
+	printf("TODOS LOS ELEMENTOS [%d]\n\n",total_Palabras);
 
 	arrypalabra(or);			
 	printf("\n DATOS : \n");
@@ -47,8 +77,6 @@ int main(int argc, char *argv[]) {
 		       printf("=>%d, %s\n", i, p[i]);
 		    }
 
-
 }
-}	
-	
+}		
 } 
